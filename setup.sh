9ff6b167bbc31ac3213fa8b81ffdc4ecb079a9fc
Ashutosh Devpura <devpura.a@northeastern.edu>
2023-04-18 18:32:00 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+# Author: Ashutosh Devpura
+# Email: ashutoshdevpura@gmail.com
+
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
